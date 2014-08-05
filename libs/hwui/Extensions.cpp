33c716cbf676d6dbb6b476f534f36ef124c77950
guoyin.chen <guoyin.chen@freescale.com>
2014-08-05 09:53:34 +0800

    ENGR00325098 2D/3D performance downgrade a lot comparing with GA
    Force hwui to use es2.0 API for DL&DQ
    Performance will down grade a lot on es3.0 in DL
    No Performance different in DQ between es3.0 and es2.0
    Improve the 2D performance from 250 to 490 in DL ARD
    
    Signed-off-by: guoyin.chen <guoyin.chen@freescale.com>
    
    Change-Id: I8992dfbea9692cd4bbd820d825dac07f8577e6cc
    Reviewed-on: http://androidsource.ap.freescale.net/project/336
    Reviewed-by: Xuegang Liu <xuegang.liu@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/937
    Reviewed-on: http://androidsource.ap.freescale.net/project/2925
    Reviewed-by: juan.hao <juan.hao@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/3226
    Reviewed-by: guoyin.chen <guoyin.chen@nxp.com>

--- libs/hwui/Extensions.cpp
+++ libs/hwui/Extensions.cpp
@@ -51,6 +51,12 @@ Extensions::Extensions() {
         mVersionMinor = 0;
     }
 
+#ifdef FSL_FORCE_USE_OPENGL_2X
+    // Fall back to openGL ES 2.x, as 3.x have performance drop in DL GC880
+    if (mVersionMajor == 3)
+        mVersionMajor = 2;
+#endif
+
     auto extensions = StringUtils::split((const char*) glGetString(GL_EXTENSIONS));
     mHasNPot = extensions.has("GL_OES_texture_npot");
     mHasFramebufferFetch = extensions.has("GL_NV_shader_framebuffer_fetch");
