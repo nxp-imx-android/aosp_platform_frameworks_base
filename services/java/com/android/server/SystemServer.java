e431f5ab53b5cb897c64d9bd347de2e29841f5e3
Haoran.Wang <elven.wang@nxp.com>
2019-01-23 20:06:06 +0800

    MA-13142-6 Split start SystemUI into two parts
    In SystemServer, start SystemUI including two parts.
    One is use Intent to start SystemUI service, another
    one is call windowManager to set UI related resources.
    
    In boot time tuning view, SystemUI service start the earlier
    the better. So split the start SystemUI into two parts.
    Move start SystemUI service part very early. This will
    make zygote prepare the process resource early and it
    will be able to finish earilier.
    
    TEST: Test boot time on imx8qxp, get 150ms saved.
    
    Change-Id: Ia245557bb3417689c4f295d4e4eb2cdda020485a
    Signed-off-by: Haoran.Wang <elven.wang@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/7263
    Reviewed-by: Xuegang Liu <xuegang.liu@nxp.com>
    Reviewed-by: guoyin.chen <guoyin.chen@nxp.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -1022,6 +1022,13 @@ public final class SystemServer {
             }
         }
 
+        try {
+            startSystemUi_0(context);
+        } catch (Throwable e) {
+            reportWtf("starting System UI part 0", e);
+        }
+
+
         // We start this here so that we update our configuration to set watch or television
         // as appropriate.
         traceBeginAndSlog("StartUiModeManager");
@@ -1846,13 +1853,11 @@ public final class SystemServer {
                 traceEnd();
             }
 
-            traceBeginAndSlog("StartSystemUI");
             try {
-                startSystemUi(context, windowManagerF);
+                startSystemUi_1(windowManagerF);
             } catch (Throwable e) {
-                reportWtf("starting System UI", e);
+                reportWtf("starting System UI part 1", e);
             }
-            traceEnd();
 
             if (!isAndroidAuto) {
                 traceBeginAndSlog("MakeNetworkManagementServiceReady");
@@ -2030,13 +2035,15 @@ public final class SystemServer {
         }
     }
 
-    static final void startSystemUi(Context context, WindowManagerService windowManager) {
+    static final void startSystemUi_0(Context context) {
         Intent intent = new Intent();
         intent.setComponent(new ComponentName("com.android.systemui",
                     "com.android.systemui.SystemUIService"));
         intent.addFlags(Intent.FLAG_DEBUG_TRIAGED_MISSING);
-        //Slog.d(TAG, "Starting service: " + intent);
         context.startServiceAsUser(intent, UserHandle.SYSTEM);
+    }
+
+    static final void startSystemUi_1(WindowManagerService windowManager) {
         windowManager.onSystemUiStarted();
     }
 
